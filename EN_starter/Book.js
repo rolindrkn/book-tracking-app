085a03c2b44a188eb3b4f5ffc1ef9f970c757ff0
hulya <hulyakarakayaa06@gmail.com>
2020-02-02 13:01:30 -0800

    modify js file

--- EN_starter/Book.js
+++ EN_starter/Book.js
@@ -20,8 +20,8 @@ export class Book {
     }
   }
 }
-const book1 =new Book ("Life 3.0", "Max Tegmark","Non-fiction", 384,300);
+const book1 =new Book ("Life 3.0", "Max Tegmark","Non-fiction", 300,384);
 const book2 = new Book ("Think with Socrates", "Paul Herrick", "Non-fiction",414,414);
-const book3 = new Book ("The Power Of Habit", "Charles Duhigg","Self-Help",375,0);
+const book3 = new Book ("The Power Of Habit", "Charles Duhigg","Self-Help",0,375);
 
 export const books = [book1, book2, book3];
